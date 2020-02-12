2b17e33d064488f55afc52b1ab8e29c66215077a
Louis <l.zerri@pepperbay.fr>
2020-02-12 21:19:12 +0100

    feat: formulaire de contact

---
+++ src/Entity/Contact.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+class Contact 
+{
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=100)
+     */
+    private $firstname;
+
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=100)
+     */
+    private $lastname;
+
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *  pattern="/[0-9]{10}/"
+     * )
+     */
+    private $phone;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=10)
+     */
+    private $message;
+
+    /**
+     * @var Property|null
+     */
+    private $property;
+
+    
+    public function getFirstName(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstName(?string $firstname): Contact
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastName(?string $lastname): Contact
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): Contact
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): Contact
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): Contact
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function getProperty(): ?Property
+    {
+        return $this->property;
+    }
+
+    public function setProperty(?Property $property): Contact
+    {
+        $this->property = $property;
+        return $this;
+    }
+
+    
+
+
+
+}
